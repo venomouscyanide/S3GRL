3cf67f8bc5a74ddbf1dadcceebad4c9947117e81
Paul Louis <iampaullouis@gmail.com>
2022-04-12 22:53:50 -0400

    Add train_percent as an argument

--- hypertuner.py
+++ hypertuner.py
@@ -20,13 +20,13 @@ class HyperTuningSearchSpace:
 class ManualTuner:
     @staticmethod
     def tune(dataset, model, hidden_channels, use_feature, lr,
-             runs, use_heuristic, m, M, dropedge, save_appendix, data_appendix, device):
+             runs, use_heuristic, m, M, dropedge, save_appendix, data_appendix, device, train_percent):
         sweal_parser = SWEALArgumentParser(dataset=dataset, fast_split=False, model=model, sortpool_k=0.6, num_layers=3,
                                            hidden_channels=hidden_channels, batch_size=32, num_hops=1,
                                            ratio_per_hop=1.0, max_nodes_per_hop=None, node_label='drnl',
                                            use_feature=use_feature, use_edge_weight=False, lr=lr, epochs=50,
-                                           runs=runs,
-                                           train_percent=100, val_percent=100, test_percent=100, dynamic_train=False,
+                                           runs=runs, train_percent=train_percent, val_percent=100, test_percent=100,
+                                           dynamic_train=False,
                                            dynamic_val=False, dynamic_test=False, num_workers=16,
                                            train_node_embedding=False, pretrained_node_embedding=False,
                                            use_valedges_as_input=False, eval_steps=1, log_steps=1,
@@ -51,6 +51,7 @@ if __name__ == '__main__':
     parser.add_argument('--use_heuristic', action='store_true')
     parser.add_argument('--save_appendix', type=str)
     parser.add_argument('--data_appendix', type=str)
+    parser.add_argument('--train_percent', type=float)
     parser.add_argument('--cuda_device', type=int, default=0, help="Only set available the passed GPU")
 
     args = parser.parse_args()
@@ -68,4 +69,5 @@ if __name__ == '__main__':
         ManualTuner.tune(dataset=args.dataset, model=args.model, hidden_channels=args.hidden_channels,
                          lr=args.lr, runs=args.runs, use_feature=args.use_feature,
                          use_heuristic=args.use_heuristic, m=perm[0], M=perm[1], dropedge=perm[2],
-                         save_appendix=args.save_appendix, data_appendix=args.data_appendix, device=device)
+                         save_appendix=args.save_appendix, data_appendix=args.data_appendix, device=device,
+                         train_percent=args.train_percent)
