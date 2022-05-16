643d3240faa683f6833e50c6c81d31b1bd079f35
Paul Louis <iampaullouis@gmail.com>
2022-05-16 00:35:01 -0400

    Enhancements on MLP

--- non_structure_aware.py
+++ non_structure_aware.py
@@ -8,7 +8,7 @@ def train_mlp(train, test_data, device, lr, dropout, epochs, dim=256):
     """
     Train for link prediction downstream task
     """
-    mlp = MLP(channel_list=[train.x.size(1), dim, dim, 1], dropout=dropout, batch_norm=False).to(device)
+    mlp = MLP(channel_list=[train.x.size(1), dim, dim, dim], dropout=dropout, batch_norm=False).to(device)
     optimizer = torch.optim.Adam(params=mlp.parameters(), lr=lr)
     criterion = torch.nn.BCEWithLogitsLoss()
 
@@ -46,8 +46,29 @@ def train_mlp(train, test_data, device, lr, dropout, epochs, dim=256):
     # forward pass on test data
     mlp.eval()
     out = mlp(test_data.x)
-    link_embedding = out[test_data.edge_label_index[0]] * out[test_data.edge_label_index[1]]
-    test_roc = roc_auc_score(test_data.edge_label.detach().cpu().numpy(), link_embedding.detach().cpu().numpy())
+
+    test_neg_edge_index = negative_sampling(
+        edge_index=test_data.edge_index, num_nodes=test_data.num_nodes,
+        num_neg_samples=test_data.edge_label_index.size(1), method='dense')
+    test_neg_edge_index.to(device)
+
+    test_edge_label_index = torch.cat(
+        [test_data.edge_label_index, test_neg_edge_index],
+        dim=-1,
+    )
+    test_edge_label_index.to(device)
+
+    test_edge_label = torch.cat([
+        test_data.edge_label,
+        test_data.edge_label.new_zeros(test_neg_edge_index.size(1))
+    ], dim=0)
+    test_edge_label.to(device)
+
+    test_data.new_edge_label_index = test_edge_label_index
+
+    link_embedding = out[test_data.new_edge_label_index[0]] * out[test_data.new_edge_label_index[1]]
+    test_roc = roc_auc_score(test_edge_label.detach().cpu().numpy(),
+                             link_embedding.sum(dim=-1).detach().cpu().numpy())
     print(f"Test AUC: {test_roc}")
 
     return test_roc
