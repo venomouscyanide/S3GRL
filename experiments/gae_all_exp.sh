f1207c483fd07d42f2036d4c3300e225c8bfcd85
Paul Louis <iampaullouis@gmail.com>
2022-05-17 01:48:14 -0400

    Bug fix ratio in Fb GAE bash

--- experiments/gae_all_exp.sh
+++ experiments/gae_all_exp.sh
@@ -9,5 +9,5 @@ python seal_link_pred.py --dataset CiteSeer --train_gae --base_gae SAGE --epochs
 python seal_link_pred.py --dataset Pubmed --train_gae --base_gae GCN --epochs 50 --split_val_ratio 0.05 --split_test_ratio 0.10 --runs 5
 python seal_link_pred.py --dataset Pubmed --train_gae --base_gae SAGE --epochs 50 --split_val_ratio 0.05 --split_test_ratio 0.10 --runs 5
 # attributed-Facebook
-python seal_link_pred.py --dataset attributed-Facebook --train_gae --base_gae GCN --epochs 50 --split_val_ratio 0.05 --split_test_ratio 0.10 --runs 5
-python seal_link_pred.py --dataset attributed-Facebook --train_gae --base_gae SAGE --epochs 50 --split_val_ratio 0.05 --split_test_ratio 0.10 --runs 5
+python seal_link_pred.py --dataset attributed-Facebook --train_gae --base_gae GCN --epochs 50 --split_val_ratio 0.05 --split_test_ratio 0.50 --runs 5
+python seal_link_pred.py --dataset attributed-Facebook --train_gae --base_gae SAGE --epochs 50 --split_val_ratio 0.05 --split_test_ratio 0.50 --runs 5
