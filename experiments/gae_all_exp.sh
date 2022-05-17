8abca4776c4e3dc2e4793337d21b3d48670422b4
Paul Louis <iampaullouis@gmail.com>
2022-05-17 01:18:02 -0400

    Reduce epochs to 50

--- experiments/gae_all_exp.sh
+++ experiments/gae_all_exp.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 # Cora
-python seal_link_pred.py --dataset Cora --train_gae --base_gae GCN --epochs 500 --split_val_ratio 0.05 --split_test_ratio 0.10 --runs 5
-python seal_link_pred.py --dataset Cora --train_gae --base_gae SAGE --epochs 500 --split_val_ratio 0.05 --split_test_ratio 0.10 --runs 5
+python seal_link_pred.py --dataset Cora --train_gae --base_gae GCN --epochs 50 --split_val_ratio 0.05 --split_test_ratio 0.10 --runs 5
+python seal_link_pred.py --dataset Cora --train_gae --base_gae SAGE --epochs 50 --split_val_ratio 0.05 --split_test_ratio 0.10 --runs 5
 # CiteSeer
-python seal_link_pred.py --dataset CiteSeer --train_gae --base_gae GCN --epochs 500 --split_val_ratio 0.05 --split_test_ratio 0.10 --runs 5
-python seal_link_pred.py --dataset CiteSeer --train_gae --base_gae SAGE --epochs 500 --split_val_ratio 0.05 --split_test_ratio 0.10 --runs 5
+python seal_link_pred.py --dataset CiteSeer --train_gae --base_gae GCN --epochs 50 --split_val_ratio 0.05 --split_test_ratio 0.10 --runs 5
+python seal_link_pred.py --dataset CiteSeer --train_gae --base_gae SAGE --epochs 50 --split_val_ratio 0.05 --split_test_ratio 0.10 --runs 5
 # Pubmed
-python seal_link_pred.py --dataset Pubmed --train_gae --base_gae GCN --epochs 500 --split_val_ratio 0.05 --split_test_ratio 0.10 --runs 5
-python seal_link_pred.py --dataset Pubmed --train_gae --base_gae SAGE --epochs 500 --split_val_ratio 0.05 --split_test_ratio 0.10 --runs 5
+python seal_link_pred.py --dataset Pubmed --train_gae --base_gae GCN --epochs 50 --split_val_ratio 0.05 --split_test_ratio 0.10 --runs 5
+python seal_link_pred.py --dataset Pubmed --train_gae --base_gae SAGE --epochs 50 --split_val_ratio 0.05 --split_test_ratio 0.10 --runs 5
 # attributed-Facebook
-python seal_link_pred.py --dataset attributed-Facebook --train_gae --base_gae GCN --epochs 500 --split_val_ratio 0.05 --split_test_ratio 0.10 --runs 5
-python seal_link_pred.py --dataset attributed-Facebook --train_gae --base_gae SAGE --epochs 500 --split_val_ratio 0.05 --split_test_ratio 0.10 --runs 5
+python seal_link_pred.py --dataset attributed-Facebook --train_gae --base_gae GCN --epochs 50 --split_val_ratio 0.05 --split_test_ratio 0.10 --runs 5
+python seal_link_pred.py --dataset attributed-Facebook --train_gae --base_gae SAGE --epochs 50 --split_val_ratio 0.05 --split_test_ratio 0.10 --runs 5
