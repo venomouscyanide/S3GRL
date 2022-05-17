323c8d121c692917b14bf5da2251e7f657e95479
Paul Louis <iampaullouis@gmail.com>
2022-05-17 08:16:36 -0400

    Run Pubmed last

--- experiments/gae_drnl.sh
+++ experiments/gae_drnl.sh
@@ -13,13 +13,6 @@ python seal_link_pred.py --dataset CiteSeer --num_hops 3 --hidden_channels 256 -
 cd ../SWEAL_OGB/
 python seal_link_pred.py --dataset CiteSeer --hidden_channels 256 --runs 5 --model GCN --m 5 --M 50 --profile
 python seal_link_pred.py --dataset CiteSeer --hidden_channels 256 --runs 5 --model SAGE --m 5 --M 50 --profile
-# Pubmed
-cd ../SEAL_OGB/
-python seal_link_pred.py --dataset PubMed --num_hops 3 --use_feature --dynamic_train --runs 5 --model GCN --profile
-python seal_link_pred.py --dataset PubMed --num_hops 3 --use_feature --dynamic_train --runs 5 --model SAGE --profile
-cd ../SWEAL_OGB/
-python seal_link_pred.py --dataset PubMed --runs 5 --model GCN --m 5 --M 50 --use_feature --profile --dynamic_train
-python seal_link_pred.py --dataset PubMed --runs 5 --model SAGE --m 5 --M 50 --use_feature --profile --dynamic_train
 # Attributed-Facebook
 cd ../SEAL_OGB/
 python seal_link_pred.py --dataset attributed-Facebook --num_hops 1 --use_feature --hidden_channels 256 --split_test_ratio 0.50 --model GCN --runs 5 --profile
@@ -34,3 +27,11 @@ python seal_link_pred.py --dataset ogbl-collab --num_hops 1 --train_percent 15 -
 cd ../SWEAL_OGB/
 python seal_link_pred.py --dataset ogbl-collab --train_percent 15 --hidden_channels 256 --use_valedges_as_input --runs 5 --model GCN --m 5 --M 20 --profile --val_percent 10
 python seal_link_pred.py --dataset ogbl-collab --train_percent 15 --hidden_channels 256 --use_valedges_as_input --runs 5 --model SAGE --m 5 --M 20 --profile --val_percent 10
+# Pubmed is last as it may get stuck due to dynamic training.
+# Pubmed
+cd ../SEAL_OGB/
+python seal_link_pred.py --dataset PubMed --num_hops 3 --use_feature --dynamic_train --runs 5 --model GCN --profile
+python seal_link_pred.py --dataset PubMed --num_hops 3 --use_feature --dynamic_train --runs 5 --model SAGE --profile
+cd ../SWEAL_OGB/
+python seal_link_pred.py --dataset PubMed --runs 5 --model GCN --m 5 --M 50 --use_feature --profile --dynamic_train
+python seal_link_pred.py --dataset PubMed --runs 5 --model SAGE --m 5 --M 50 --use_feature --profile --dynamic_train
