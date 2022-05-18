3e208b4c8b070d2dcceaf61560328b3a2e34a1c4
Paul Louis <iampaullouis@gmail.com>
2022-05-18 12:49:18 -0400

    Add cli for seal sweal on ogbl-collab

---
+++ experiments/collab_seal_sweal.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# ogbl-collab
+cd SEAL_OGB/
+python seal_link_pred.py --dataset ogbl-collab --num_hops 1 --train_percent 15 --hidden_channels 256 --use_valedges_as_input --runs 5 --profile --val_percent 10
+cd ../SWEAL_OGB/
+python seal_link_pred.py --dataset ogbl-collab --train_percent 15 --hidden_channels 256 --use_valedges_as_input --runs 5 --model DGCNN --m 5 --M 20 --profile --val_percent 10
