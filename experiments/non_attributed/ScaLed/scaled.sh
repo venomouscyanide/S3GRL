a3bd719b77d3068b4a54a93e8e65902c35c03fe7
Paul Louis <iampaullouis@gmail.com>
2022-05-30 13:23:22 -0400

    Update all m and M to 2, 20

--- experiments/non_attributed/ScaLed/scaled.sh
+++ experiments/non_attributed/ScaLed/scaled.sh
@@ -4,40 +4,40 @@
 
 # USAir
 for SEED in 1 2 3 4 5; do
-  python seal_link_pred.py --dataset USAir --epochs 50 --m 5 --M 20 --seed $SEED --profile
+  python seal_link_pred.py --dataset USAir --epochs 50 --m 2 --M 20 --seed $SEED --profile
 done
 
 # NS
 for SEED in 1 2 3 4 5; do
-  python seal_link_pred.py --dataset NS --epochs 50 --m 2 --M 5 --seed $SEED --profile
+  python seal_link_pred.py --dataset NS --epochs 50 --m 2 --M 20 --seed $SEED --profile
 done
 
 # Power
 for SEED in 1 2 3 4 5; do
-  python seal_link_pred.py --dataset Power --epochs 50 --m 2 --M 5 --seed $SEED --profile
+  python seal_link_pred.py --dataset Power --epochs 50 --m 2 --M 20 --seed $SEED --profile
 done
 
 # Celegans
 for SEED in 1 2 3 4 5; do
-  python seal_link_pred.py --dataset Celegans --epochs 50 --m 5 --M 10 --seed $SEED --profile
+  python seal_link_pred.py --dataset Celegans --epochs 50 --m 2 --M 20 --seed $SEED --profile
 done
 
 # Router
 for SEED in 1 2 3 4 5; do
-  python seal_link_pred.py --dataset Router --epochs 50 --m 3 --M 5 --seed $SEED --profile
+  python seal_link_pred.py --dataset Router --epochs 50 --m 2 --M 20 --seed $SEED --profile
 done
 
 # PB
 for SEED in 1 2 3 4 5; do
-  python seal_link_pred.py --dataset PB --epochs 50 --m 3 --M 20 --seed $SEED --profile
+  python seal_link_pred.py --dataset PB --epochs 50 --m 2 --M 20 --seed $SEED --profile
 done
 
 # Ecoli
 for SEED in 1 2 3 4 5; do
-  python seal_link_pred.py --dataset Ecoli --epochs 50 --m 5 --M 10 --seed $SEED --profile
+  python seal_link_pred.py --dataset Ecoli --epochs 50 --m 2 --M 20 --seed $SEED --profile
 done
 
 # Yeast
 for SEED in 1 2 3 4 5; do
-  python seal_link_pred.py --dataset Yeast --epochs 50 --m 3 --M 20 --seed $SEED --profile
+  python seal_link_pred.py --dataset Yeast --epochs 50 --m 2 --M 20 --seed $SEED --profile
 done
