02d1fc2c0933d9acb81a225616dbaf9da880b366
Shweta Jacob <shwetaaj59@gmail.com>
2022-05-30 02:04:44 -0400

    Convert model size to mb in parser

--- parsers/seal_attributed.py
+++ parsers/seal_attributed.py
@@ -2,6 +2,7 @@ import argparse
 import json
 
 import numpy as np
+from torch_geometric.profile.utils import byte_to_megabyte
 
 seal_dict = \
     {
@@ -10,6 +11,7 @@ seal_dict = \
              'AP': [],
              'Time taken (per run)': [],
              'Max allocated CUDA': [],
+             'Max Nvidia Smi used CUDA': [],
              'Model size': [],
              'Parameters': []
              },
@@ -18,6 +20,7 @@ seal_dict = \
              'AP': [],
              'Time taken (per run)': [],
              'Max allocated CUDA': [],
+             'Max Nvidia Smi used CUDA': [],
              'Model size': [],
              'Parameters': []
              }
@@ -30,6 +33,7 @@ scaled_dict = \
              'AP': [],
              'Time taken (per run)': [],
              'Max allocated CUDA': [],
+             'Max Nvidia Smi used CUDA': [],
              'Model size': [],
              'Parameters': []
              },
@@ -38,6 +42,7 @@ scaled_dict = \
              'AP': [],
              'Time taken (per run)': [],
              'Max allocated CUDA': [],
+             'Max Nvidia Smi used CUDA': [],
              'Model size': [],
              'Parameters': []
              }
@@ -65,14 +70,16 @@ def parse_attributed_results(file_name, method_type):
                 line = results[index]
                 if line.startswith("Summarized stats:"):
                     max_allocated_cuda = float(line.split("max_allocated_cuda=")[-1].split(f',')[0].strip())
+                    max_nvidia_smi_cuda = float(line.split("max_nvidia_smi_used_cuda=")[-1].split(f')')[0].strip())
                     result_dict[dataset]['Max allocated CUDA'].append(max_allocated_cuda)
+                    result_dict[dataset]['Max Nvidia Smi used CUDA'].append(max_nvidia_smi_cuda)
                     index += 1
                 if line.startswith("Model size"):
                     model_size = float(line.split("Model size: ")[-1])
                     index += 1
                     line = results[index]
                     parameters = float(line.split("Parameters: ")[-1])
-                    result_dict[dataset]['Model size'].append(model_size)
+                    result_dict[dataset]['Model size'].append(byte_to_megabyte(model_size))
                     result_dict[dataset]['Parameters'].append(parameters)
                 if line.startswith("All runs:"):
                     index += 4
@@ -100,12 +107,14 @@ def parse_attributed_results(file_name, method_type):
         ap = np.array(result_key['AP'])
         time_per_run = np.array(result_key['Time taken (per run)'])
         allocated_cuda = np.array(result_key['Max allocated CUDA'])
+        max_nvidia_smi_cuda = np.array(result_key['Max Nvidia Smi used CUDA'])
         model_size = np.array(result_key['Model size'])
         parameters = np.array(result_key['Parameters'])
         result_key['AUC Mean'] = f'{auc.mean():.2f} ± {auc.std():.2f}'
         result_key['AP Mean'] = f'{ap.mean():.2f} ± {ap.std():.2f}'
         result_key['Time taken (per run) Mean'] = f'{time_per_run.mean():.2f} ± {time_per_run.std():.2f}'
         result_key['Max allocated CUDA Mean'] = f'{allocated_cuda.mean():.2f} ± {allocated_cuda.std():.2f}'
+        result_key['Max Nvidia Smi used CUDA Mean'] = f'{max_nvidia_smi_cuda.mean():.2f} ± {max_nvidia_smi_cuda.std():.2f}'
         result_key['Model size Mean'] = f'{model_size.mean():.2f} ± {model_size.std():.2f}'
         result_key['Parameters Mean'] = f'{parameters.mean():.2f} ± {parameters.std():.2f}'
     with open(f'attributed-{method_type}-result.json', 'w', encoding='utf-8') as fp:
