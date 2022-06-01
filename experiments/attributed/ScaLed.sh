e5fed93b8db5215f476dfc447f08561c775e7284
Paul Louis <iampaullouis@gmail.com>
2022-06-01 06:08:50 -0400

    Update scaled exp on attributed

--- experiments/attributed/ScaLed.sh
+++ experiments/attributed/ScaLed.sh
@@ -2,10 +2,10 @@
 # run ScaLed on Cora and CiteSeer to show performance on attributed datasets
 # CORA
 for SEED in 1 2 3 4 5; do
-  python seal_link_pred.py --dataset Cora --m 5 --M 50 --use_feature --profile --seed $SEED
+  python seal_link_pred.py --dataset Cora --m 3 --M 20 --use_feature --profile --seed $SEED
 done
 
 # CITESEER
 for SEED in 1 2 3 4 5; do
-  python seal_link_pred.py --dataset CiteSeer --m 5 --M 50 --use_feature --profile --seed $SEED
+  python seal_link_pred.py --dataset CiteSeer --m 3 --M 20 --use_feature --profile --seed $SEED
 done
