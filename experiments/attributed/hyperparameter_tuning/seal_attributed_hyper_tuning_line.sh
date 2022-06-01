1b8a72938fad95af430f2d00ab645cf35e1a3eeb
Paul Louis <iampaullouis@gmail.com>
2022-06-01 12:26:19 -0400

    Add exps for Cora sensitivity analysis as well

---
+++ experiments/attributed/hyperparameter_tuning/seal_attributed_hyper_tuning_line.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# constant line for SEAL for hypertuning results
+
+# Cora
+for SEED in 1 2 3 4 5; do
+  python seal_link_pred.py --dataset Cora --epochs 5 --num_hops 3 --use_feature --seed $SEED
+done
