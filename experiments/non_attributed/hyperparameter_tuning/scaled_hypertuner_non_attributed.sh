5e1c7572024d1bdfaff3d63d089e333abae58a65
Shweta Jacob <shwetaaj59@gmail.com>
2022-06-03 17:23:11 -0400

    Update hypertuner script for non-attributed and search space

--- experiments/non_attributed/hyperparameter_tuning/scaled_hypertuner_non_attributed.sh
+++ experiments/non_attributed/hyperparameter_tuning/scaled_hypertuner_non_attributed.sh
@@ -2,5 +2,11 @@
 # seed is fixed to [1,2,3,4,5] which is same as training seed
 # for now, to reproduce the results, just run this bash script, copy the terminal output log and parse it using parser
 # the parsed data is placed inside plot.py to create the graphs
-python hypertuner.py --model DGCNN --dataset PB --hidden_channels 32 --lr 0.0001 --epochs 5 --runs 1 --hyper_runs 5 --save_appendix "" --data_appendix "" --train_percent 100 --seed 1
+python hypertuner.py --model DGCNN --dataset USAir --hidden_channels 32 --lr 0.0001 --epochs 5 --runs 1 --hyper_runs 5 --save_appendix "" --data_appendix "" --train_percent 100 --seed 1
+python hypertuner.py --model DGCNN --dataset NS --hidden_channels 32 --lr 0.0001 --epochs 5 --runs 1 --hyper_runs 5 --save_appendix "" --data_appendix "" --train_percent 100 --seed 1
+python hypertuner.py --model DGCNN --dataset Power --hidden_channels 32 --lr 0.0001 --epochs 5 --runs 1 --hyper_runs 5 --save_appendix "" --data_appendix "" --train_percent 100 --seed 1
+python hypertuner.py --model DGCNN --dataset Celegans --hidden_channels 32 --lr 0.0001 --epochs 5 --runs 1 --hyper_runs 5 --save_appendix "" --data_appendix "" --train_percent 100 --seed 1
+python hypertuner.py --model DGCNN --dataset Router --hidden_channels 32 --lr 0.0001 --epochs 5 --runs 1 --hyper_runs 5 --save_appendix "" --data_appendix "" --train_percent 100 --seed 1
+python hypertuner.py --model DGCNN --dataset Yeast --hidden_channels 32 --lr 0.0001 --epochs 5 --runs 1 --hyper_runs 5 --save_appendix "" --data_appendix "" --train_percent 100 --seed 1
 python hypertuner.py --model DGCNN --dataset Ecoli --hidden_channels 32 --lr 0.0001 --epochs 5 --runs 1 --hyper_runs 5 --save_appendix "" --data_appendix "" --train_percent 100 --seed 1
+python hypertuner.py --model DGCNN --dataset PB --hidden_channels 32 --lr 0.0001 --epochs 5 --runs 1 --hyper_runs 5 --save_appendix "" --data_appendix "" --train_percent 100 --seed 1
