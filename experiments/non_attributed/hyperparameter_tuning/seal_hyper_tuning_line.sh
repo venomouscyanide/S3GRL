0bb72fcf8dcafec323dcb9a46bce557a189cf579
Shweta Jacob <shwetaaj59@gmail.com>
2022-06-04 21:07:06 -0400

    Update SEAL hypertuner script for non-attributed

--- experiments/non_attributed/hyperparameter_tuning/seal_hyper_tuning_line.sh
+++ experiments/non_attributed/hyperparameter_tuning/seal_hyper_tuning_line.sh
@@ -1,12 +1,42 @@
 #!/bin/bash
 # constant line for SEAL for hypertuning results
 
-# PB
+# USAir
 for SEED in 1 2 3 4 5; do
-  python seal_link_pred.py --dataset PB --epochs 5 --num_hops 2 --seed $SEED
+  python seal_link_pred.py --dataset USAir --epochs 5 --num_hops 2 --seed $SEED
+done
+
+# NS
+for SEED in 1 2 3 4 5; do
+  python seal_link_pred.py --dataset NS --epochs 5 --num_hops 2 --seed $SEED
+done
+
+# Power
+for SEED in 1 2 3 4 5; do
+  python seal_link_pred.py --dataset Power --epochs 5 --num_hops 2 --seed $SEED
+done
+
+# Celegans
+for SEED in 1 2 3 4 5; do
+  python seal_link_pred.py --dataset Celegans --epochs 5 --num_hops 2 --seed $SEED
+done
+
+# Router
+for SEED in 1 2 3 4 5; do
+  python seal_link_pred.py --dataset Router --epochs 5 --num_hops 2 --seed $SEED
+done
+
+# Yeast
+for SEED in 1 2 3 4 5; do
+  python seal_link_pred.py --dataset Yeast --epochs 5 --num_hops 2 --seed $SEED
 done
 
 # Ecoli
 for SEED in 1 2 3 4 5; do
   python seal_link_pred.py --dataset Ecoli --epochs 5 --num_hops 2 --seed $SEED
+done
+
+# PB
+for SEED in 1 2 3 4 5; do
+  python seal_link_pred.py --dataset PB --epochs 5 --num_hops 2 --seed $SEED
 done
