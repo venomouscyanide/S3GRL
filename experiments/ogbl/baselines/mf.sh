4bebe294a3cd0872b4f99fed7367bbf6fc40b0b0
Paul Louis <iampaullouis@gmail.com>
2022-06-05 00:18:52 -0400

    Change number of epochs in collab mf exp

--- experiments/ogbl/baselines/mf.sh
+++ experiments/ogbl/baselines/mf.sh
@@ -2,5 +2,5 @@
 
 # ogbl-collab
 for SEED in 1 2 3 4 5; do
-  python seal_link_pred.py --dataset ogbl-collab --epochs 50 --train_mf --hidden_channels 256 --lr 0.01 --seed $SEED --batch_size 65536
+  python seal_link_pred.py --dataset ogbl-collab --epochs 200 --train_mf --hidden_channels 256 --lr 0.01 --seed $SEED --batch_size 65536
 done
