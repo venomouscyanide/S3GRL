2c4d08ed4fb51892c2b76e4a06f1dea6df836af3
Shweta Jacob <shwetaaj59@gmail.com>
2022-06-06 04:29:48 -0400

    Add train percent to calc ratio script

--- experiments/ogbl/calc_ratio/calc_ratio.sh
+++ experiments/ogbl/calc_ratio/calc_ratio.sh
@@ -3,5 +3,5 @@
 
 # ogbl-collab
 for SEED in 1 2 3 4 5; do
-  python seal_link_pred.py --dataset ogbl-collab --num_hops 1 --m 1 --M 20 --use_feature --calc_ratio --seed $SEED
+  python seal_link_pred.py --dataset ogbl-collab --num_hops 1 --m 1 --M 20 --use_feature --calc_ratio --train_percent 15 --seed $SEED
 done
