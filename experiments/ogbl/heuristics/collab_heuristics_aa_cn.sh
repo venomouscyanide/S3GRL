b093b21a5fca6b55076954ffcc151aebbe4b57d2
Paul Louis <iampaullouis@gmail.com>
2022-06-10 08:37:26 -0400

    Update experiment for PPR, AA, CN on ogbl-collab with val data as input

--- experiments/ogbl/heuristics/collab_heuristics_aa_cn.sh
+++ experiments/ogbl/heuristics/collab_heuristics_aa_cn.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
-# this will help reproduce scaled vs baselines results for heuristics; AA, CN on ogbl-collab
+# this will help reproduce scaled vs baselines results for heuristics; AA, CN, PPR on ogbl-collab
 # all runs are on 5 different seeds(1 run each of every seed).
 
 # CN
-for SEED in 1 2 3 4 5; do
-  python seal_link_pred.py --dataset ogbl-collab --use_heuristic CN --seed $SEED
+for SEED in 1; do
+  python seal_link_pred.py --dataset ogbl-collab --use_heuristic CN --seed $SEED --use_valedges_as_input
 done
 
 # AA
-for SEED in 1 2 3 4 5; do
-  python seal_link_pred.py --dataset ogbl-collab --use_heuristic AA --seed $SEED
+for SEED in 1; do
+  python seal_link_pred.py --dataset ogbl-collab --use_heuristic AA --seed $SEED --use_valedges_as_input
+done
+
+# PPR
+for SEED in 1; do
+  python seal_link_pred.py --dataset ogbl-collab --use_heuristic PPR --seed $SEED --use_valedges_as_input
 done
