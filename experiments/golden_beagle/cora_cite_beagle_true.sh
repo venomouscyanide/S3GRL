c7a66520ab9701fce5fe7462cc71245738c8c47c
Paul Louis <iampaullouis@gmail.com>
2022-09-14 00:04:23 -0400

    Make num_hops 2

--- experiments/golden_beagle/cora_cite_beagle_true.sh
+++ experiments/golden_beagle/cora_cite_beagle_true.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-# TODO; uncomment the 3, 1 exps
+# TODO; uncomment the 3, 1 exps. num_hops is 2 as OOM
 # CORA
 for SEED in 1 2 3 4 5; do
-  python seal_link_pred.py --dataset Cora --epochs 50 --seed $SEED --node_label zo --use_feature --hidden_channels 256 --sign_k 3 --num_hops 3 --sign_type beagle --data_appendix cora_k1_$SEED --model SIGN --pool_operatorwise
+  python seal_link_pred.py --dataset Cora --epochs 50 --seed $SEED --node_label zo --use_feature --hidden_channels 256 --sign_k 3 --num_hops 2 --sign_type beagle --data_appendix cora_k1_$SEED --model SIGN --pool_operatorwise
 done
 rm -rf dataset/
 #for SEED in 1 2 3 4 5; do
@@ -12,7 +12,7 @@ rm -rf dataset/
 
 # CITESEER
 for SEED in 1 2 3 4 5; do
-  python seal_link_pred.py --dataset CiteSeer --epochs 50 --seed $SEED --node_label zo --use_feature --hidden_channels 256 --sign_k 3 --num_hops 3 --sign_type beagle --data_appendix citeseer_k1_$SEED --model SIGN --pool_operatorwise
+  python seal_link_pred.py --dataset CiteSeer --epochs 50 --seed $SEED --node_label zo --use_feature --hidden_channels 256 --sign_k 3 --num_hops 2 --sign_type beagle --data_appendix citeseer_k1_$SEED --model SIGN --pool_operatorwise
 done
 rm -rf dataset/
 
