a8e5559a834d54839c70fac5b686df976f09751d
Paul Louis <iampaullouis@gmail.com>
2022-09-23 17:10:33 -0400

    Bug fix

--- utils.py
+++ utils.py
@@ -424,12 +424,15 @@ def extract_enclosing_subgraphs(link_index, A, x, y, num_hops, node_label='drnl'
             normalized_powers_of_A = []
             g_h_global_list = []
 
-            for pow_of_A in powers_of_A:
-                deg = pow_of_A.sum(dim=1).to(torch.float)
-                deg_inv_sqrt = deg.pow(-0.5)
-                deg_inv_sqrt[deg_inv_sqrt == float('inf')] = 0
-                normalized_A = deg_inv_sqrt.view(-1, 1) * pow_of_A * deg_inv_sqrt.view(1, -1)
-                normalized_powers_of_A.append(normalized_A)
+            for index in range(len(powers_of_A)):
+                normalized_powers_of_A.append(torch.tensor(powers_of_A[index].todense()))
+
+            # for pow_of_A in powers_of_A:
+            #     deg = pow_of_A.sum(dim=1).to(torch.float)
+            #     deg_inv_sqrt = deg.pow(-0.5)
+            #     deg_inv_sqrt[deg_inv_sqrt == float('inf')] = 0
+            #     normalized_A = deg_inv_sqrt.view(-1, 1) * pow_of_A * deg_inv_sqrt.view(1, -1)
+            #     normalized_powers_of_A.append(normalized_A)
 
             list_of_training_edges = link_index.t().tolist()
             num_training_egs = len(list_of_training_edges)
@@ -457,7 +460,7 @@ def extract_enclosing_subgraphs(link_index, A, x, y, num_hops, node_label='drnl'
                     h_src = normalized_powers_of_A[index][src][src] + normalized_powers_of_A[index][src][dst]
                     h_dst = normalized_powers_of_A[index][dst][dst] + normalized_powers_of_A[index][dst][src]
                     g_h_global_list[index][link_number] = torch.hstack([h_src, g_global_list[index][link_number]])
-                    g_h_global_list[index][link_number + 1] = torch.hstack([h_dst, g_global_list[index][link_number]])
+                    g_h_global_list[index][link_number + 1] = torch.hstack([h_dst, g_global_list[index][link_number + 1]])
 
             for link_number in range(0, num_training_egs * 2, 2):
                 src, dst = list_of_training_edges[int(link_number / 2)]
