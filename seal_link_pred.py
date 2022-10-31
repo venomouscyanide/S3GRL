722d55889152da45bb0638c6eaf0c44b063200fd
Paul Louis <iampaullouis@gmail.com>
2022-10-31 13:35:03 -0400

    Bug fix

--- seal_link_pred.py
+++ seal_link_pred.py
@@ -1468,6 +1468,7 @@ if __name__ == '__main__':
         raise Exception(f"Cannot run hybrid mode with optimize_size set to {args.optimize_sign}")
 
     if args.sign_type == 'SuP' and args.optimize_sign and args.m and args.M:
+        torch.cuda.is_available = lambda: False
         torch.multiprocessing.set_sharing_strategy('file_system')
         torch.multiprocessing.set_start_method('spawn', force=True)
     if args.profile:
