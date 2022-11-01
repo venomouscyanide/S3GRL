1abc956bbd279dd5de345b9b911ced2692cbb803
Paul Louis <iampaullouis@gmail.com>
2022-10-31 23:44:40 -0400

    Try

--- tuned_SIGN.py
+++ tuned_SIGN.py
@@ -163,7 +163,7 @@ class OptimizedSignOperations:
             args.append((src, dst, num_hops, A, ratio_per_hop, max_nodes_per_hop, directed, A_csc, x, y,
                          sign_kwargs, rw_kwargs))
 
-        cpu_count = 8
+        cpu_count = 1
 
         print(f"Calculating SuP data using {cpu_count} parallel processes")
 
