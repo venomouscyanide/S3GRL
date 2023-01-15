deb3758514de88fe67ddcab8b793d427c149881e
Paul Louis <iampaullouis@gmail.com>
2023-01-15 00:25:24 -0500

    Plotting sensitivity codes

--- Plots/hyper_param_plots.py
+++ Plots/hyper_param_plots.py
@@ -1,5 +1,11 @@
+import warnings
+from matplotlib import MatplotlibDeprecationWarning
+
+warnings.simplefilter('ignore', MatplotlibDeprecationWarning)
+
 import argparse
 import json
+
 from collections import defaultdict
 
 import matplotlib.pyplot as plt
@@ -11,14 +17,16 @@ plt.rcParams['ps.fonttype'] = 42
 
 
 def plot_helper(result_json):
-    with open(result_json, 'rb') as results_json:
-        results_dict = json.load(results_json)
-
+    with open('sensitivity_graph_data_pos.json', 'rb') as results_json:
+        results_dict_ksup = json.load(results_json)
+    with open('sensitivity_graph_data_k_sup.json', 'rb') as results_json:
+        results_dict_pos = json.load(results_json)
+    results_dict_ksup.update(results_dict_pos)
+    results_dict = results_dict_ksup
     # multi-line graph plots for hyperparameter tuning results
     all_plot_data = defaultdict(list)
     for identifier, values in results_dict.items():
-        key = "_".join(identifier.split('_SuP')[0].split('_')[1:][::-1])
-        key = f"{identifier.split('_')[0]}_{key.split('_')[0]}"
+        key = identifier.split('_')[0] + '_' + identifier.split('_')[1] + '_' + identifier.split('_')[-2]
         dataset_prep_time = float(values['results']['Average Dataset Prep Time'].split('±')[0])
         acc = float(values['results']['Average Test AUC'].split('±')[0])
         all_plot_data[key] += [(acc, dataset_prep_time)]
@@ -38,30 +46,43 @@ def plot_ds(y_label, data_index, dataset_name, all_plot_data):
     marker_style = ['D', 's', 'o', '^']
 
     f = plt.figure()
-    x = [1, 5, 10, 15, 20]
-    p = [2, 3, 4, 5]
+    x = [1, 2, 3, 4, 5]
     default_x_ticks = range(len(x))
     plt.rcParams.update({'font.size': 16.5})
     plt.xticks(default_x_ticks, x)
 
-    # plt.yticks(np.arange(0, 99, 0.5))
-    # plt.ylim(79)
+    if y_label != "Dataset Prep. Time (sec.)":
+        plt.yticks(np.arange(80, 99, 1))
+        plt.ylim(85)
+    else:
+        plt.yticks(np.arange(15, 200, 10))
+        # plt.ylim(150)
 
     all_plot_data = {k: v for k, v in filter(lambda x: x[0].split('_')[0] == dataset_name, all_plot_data.items())}
     for index, (dataset, results) in enumerate(all_plot_data.items()):
-        dataset = dataset.split('_')[0]
+        dataset_name = dataset.split('_')[0]
+        model_name = dataset.split('_')[1]
+        if model_name == 'K':
+            model_name = "SuP+"
 
         y_plots = list(map(lambda x: x[data_index], results))
-
-        plt.plot(default_x_ticks, y_plots, label=f"p={p[index]}",
+        if model_name == 'PoS':
+            index = 1
+        plt.plot(default_x_ticks, y_plots, label=f"{model_name} h{index}",
                  linestyle=line_style[index],
                  marker=marker_style[index], linewidth=2, markersize=10)
 
     plt.ylabel(y_label, labelpad=0)
-    plt.xlabel('No. Nodes in pth Operator', labelpad=0)
-    plt.legend(loc="best", ncol=2, borderpad=0.2, labelspacing=0.1, borderaxespad=0.1)
+    plt.xlabel('Num. Operators (R)', labelpad=0)
+    if y_label != "Dataset Prep. Time (sec.)":
+        file_name = 'auc'
+        plt.legend(loc="lower left", ncol=2, borderpad=0.2, labelspacing=0.1, borderaxespad=0.1, columnspacing=0.8)
+    else:
+        file_name = 'ds_prep'
+        plt.legend(loc="upper left", ncol=2, borderpad=0.2, labelspacing=0.1, borderaxespad=0.1, columnspacing=0.8)
+
     plt.show()
-    f.savefig(f"{dataset}_hypertuner_{y_label.lower()}.pdf", bbox_inches='tight')
+    f.savefig(f"{dataset_name.lower()}_{file_name}.pdf", bbox_inches='tight')
 
 
 if __name__ == '__main__':
