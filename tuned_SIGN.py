ee4c08b0a0870bd53c8b450726bfd2248c2a332c
Paul Louis <iampaullouis@gmail.com>
2023-01-19 15:53:45 -0500

    Suppress prints

--- tuned_SIGN.py
+++ tuned_SIGN.py
@@ -60,15 +60,15 @@ class OptimizedSignOperations:
         list_of_training_edges = link_index.t().tolist()
         num_training_egs = len(list_of_training_edges)
 
-        print("Setting up A Global List")
+        # print("Setting up A Global List")
         for index, power_of_a in enumerate(normalized_powers_of_A, start=0):
-            print(f"Constructing A[{index}]")
+            # print(f"Constructing A[{index}]")
             a_global_list.append(
                 dok_matrix((num_training_egs * 2, A.shape[0]), dtype=np.float32)
             )
             power_of_a_scipy_lil = power_of_a.to_scipy().tolil()
             list_of_lilmtrx = []
-            for link_number in tqdm(range(0, num_training_egs * 2, 2), ncols=70):
+            for link_number in range(0, num_training_egs * 2, 2):
                 src, dst = list_of_training_edges[int(link_number / 2)]
                 interim_src = power_of_a_scipy_lil.getrow(src)
                 interim_src[0, dst] = 0
@@ -78,8 +78,8 @@ class OptimizedSignOperations:
                 list_of_lilmtrx.append(interim_dst)
 
             to_update = a_global_list[index]
-            print("Converting to DOK")
-            for overall_row, item in tqdm(enumerate(list_of_lilmtrx), ncols=70):
+            # print("Converting to DOK")
+            for overall_row, item in enumerate(list_of_lilmtrx):
                 data = item.data
                 rows = item.rows
 
@@ -88,10 +88,10 @@ class OptimizedSignOperations:
             idx, values = from_scipy(a_global_list[index])
             a_global_list[index] = torch.sparse_coo_tensor(idx, values, size=[num_training_egs * 2, A.shape[0]],
                                                            dtype=torch.float32)
-        print("Setting up G Global List")
+        # print("Setting up G Global List")
         original_x = x.detach()
         x = x.to_sparse()
-        for operator_id in tqdm(range(len(normalized_powers_of_A)), ncols=70):
+        for operator_id in range(len(normalized_powers_of_A)):
             mult_index, mult_value = spspmm(a_global_list[operator_id].coalesce().indices(),
                                             a_global_list[operator_id].coalesce().values(), x.indices(),
                                             x.values(), a_global_list[0].size()[0], a_global_list[0].size()[1],
@@ -99,11 +99,11 @@ class OptimizedSignOperations:
             g_global_list.append(torch.sparse_coo_tensor(mult_index, mult_value, size=[a_global_list[0].size()[0],
                                                                                        x.size()[-1]]).to_dense())
 
-        print("Setting up G H Global List")
+        # print("Setting up G H Global List")
         for index, src_dst_x in enumerate(g_global_list, start=0):
             g_h_global_list.append(torch.empty(size=[num_training_egs * 2, g_global_list[index].shape[-1] + 1]))
-            print(f"Setting up G H Global [{index}]")
-            for link_number in tqdm(range(0, num_training_egs * 2, 2), ncols=70):
+            # print(f"Setting up G H Global [{index}]")
+            for link_number in range(0, num_training_egs * 2, 2):
                 src, dst = list_of_training_edges[int(link_number / 2)]
                 h_src = normalized_powers_of_A[index][src, src].to_dense()
                 h_dst = normalized_powers_of_A[index][dst, dst].to_dense()
@@ -112,9 +112,9 @@ class OptimizedSignOperations:
                 g_h_global_list[index][link_number + 1] = torch.hstack(
                     [h_dst[0], g_global_list[index][link_number + 1]])
 
-        print("Finishing Prep with creation of data")
+        # print("Finishing Prep with creation of data")
         x = original_x
-        for link_number in tqdm(range(0, num_training_egs * 2, 2), ncols=70):
+        for link_number in range(0, num_training_egs * 2, 2):
             src, dst = list_of_training_edges[int(link_number / 2)]
             data = Data(
                 x=torch.hstack(
