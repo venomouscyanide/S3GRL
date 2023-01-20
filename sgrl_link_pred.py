49a6e50590b7218e94a2b719b5607d280feef32c
Paul Louis <iampaullouis@gmail.com>
2023-01-19 21:52:26 -0500

    Bug fix

--- sgrl_link_pred.py
+++ sgrl_link_pred.py
@@ -944,9 +944,12 @@ def run_sgrl_learning(args, device, hypertuning=False):
             val_edge_index = to_undirected(val_edge_index)
         data.edge_index = torch.cat([data.edge_index, val_edge_index], dim=-1)
         split_edge['train']['edge'] = data.edge_index.t()
-        if data.edge_weight.nelement():
-            val_edge_weight = torch.ones([val_edge_index.size(1), 1], dtype=int)
-            data.edge_weight = torch.cat([data.edge_weight, val_edge_weight], 0)
+        try:
+            if torch.any(data.edge_weight):
+                val_edge_weight = torch.ones([val_edge_index.size(1), 1], dtype=int)
+                data.edge_weight = torch.cat([data.edge_weight, val_edge_weight], 0)
+        except Exception as e:
+            print(str(e), "passing edge weight setting")
 
     if init_features:
         print(f"Init features using: {init_features}")
@@ -990,12 +993,10 @@ def run_sgrl_learning(args, device, hypertuning=False):
         else:
             raise NotImplementedError(f"init_representation: {init_representation} not supported.")
 
-    if init_representation or init_features:
+    if init_representation or init_features or args.use_feature:
         norm = NormalizeFeatures()
         transformed_data = norm(data)
         data.x = transformed_data.x
-
-
 
     evaluator = None
     if args.dataset.startswith('ogbl'):
